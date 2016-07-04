547b74cf324a63a588280ad9e9cd3b0c429fad06
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-04 16:36:10 -0500

    setup board

--- lib/knight_moves.rb
+++ lib/knight_moves.rb
@@ -1,6 +1,57 @@
-class Board
+DIM_X, DIM_Y = [(0..7).to_a] * 2
+NAME = Hash[DIM_Y.product(DIM_X).collect { |y, x| [[y, x], ('A'.upto('H').to_a[x] + 8.downto(1).to_a[y].to_s).to_sym] }]
 
+def TO_NAME(y, x)
+  NAME[[y, x]]
 end
+
+class Board
+  attr_reader :cells
+
+  def initialize
+    @cells = NAME.keys.collect do |y, x|
+      Cell.new y: y, x: x
+    end
+
+    @cells.each do |cell|
+      adjacent_cells = { 'up=': [cell.y - 1, cell.x],
+                         'right=': [cell.y, cell.x + 1],
+                         'down=': [cell.y + 1, cell.x],
+                         'left=': [cell.y, cell.x - 1], }
+      adjacent_cells
+          .collect { |k, v| [k, TO_NAME(*v)] }
+          .reject { |_, v| v.nil? }
+          .collect { |k, v| [k, @cells.find { |c| c.name == v }] }
+          .each { |k, v| cell.send(k, v) }
+
+    end
+  end
+end
+
 class Cell
+  attr_reader :name, :x, :y
   attr_accessor :value, :up, :down, :left, :right
+
+  def initialize(y: nil, x: nil, name: nil, value: nil)
+    @y = y
+    @x = x
+    @value = value
+    @name = name || TO_NAME(@y, @x)
+    @up, @right, @down, @left = [nil]*4
+  end
+
+  def inspect
+    s = []
+    s << "*: #{to_s}"
+    s << "u: #{@up.to_s}" if @up
+    s << "r: #{@right.to_s}" if @right
+    s << "d: #{@down.to_s}" if @down
+    s << "l: #{@left.to_s}" if @left
+    "{#{s.join(', ')}}"
+  end
+
+  def to_s
+    value = @value ? ":{#{@value}" : nil
+    "<#{@name}#{value}>"
+  end
 end
--- spec/lib/knight_moves_spec.rb
+++ spec/lib/knight_moves_spec.rb
@@ -1,19 +1,27 @@
 require 'knight_moves'
 
 describe 'Knight Moves' do
-  xdescribe Board do
-    context "New Board" do
-      subject { Board.new }
-      it { is_expected.to be true }
+  describe Board do
+    context Board.new do
+      its('cells.length') { should eq 64 }
     end
   end
 
   describe Cell do
-    context "#initialize" do
-      subject { Cell.new }
+    context '#initialize' do
+      subject { Cell.new y: 4, x: 3 }
       its(:value) { should be_nil }
-      it { should respond_to :up, :down, :left, :right }
+      it { should respond_to :name, :x, :y, :value, :up, :down, :left, :right }
+      its(:name) { should eq :D4 }
+      its(:x) { should be 3 }
+      its(:y) { should be 4 }
     end
+  end
+
+  describe NAME do
+    its([[0, 0]]) { should be :A8 }
+    its([[3, 4]]) { should be :E5 }
+    its([[9, 9]]) { should be_nil }
   end
 end
 
