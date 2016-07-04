269ff7421dbe7223e69d8b13edfab00d3404703b
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-04 07:52:13 -0500

    implement binary tree and search algorithms

---
+++ lib/binary_tree.rb
@@ -0,0 +1,99 @@
+class Node
+  attr_accessor :value, :up, :left, :right
+
+  def initialize(value=nil, up: nil, left: nil, right: nil)
+    @value = value
+    @up = up
+    @left = left
+    @right = right
+  end
+
+  def fork(value)
+    # puts [value, @value].to_s
+
+    if value <= @value
+      if @left === nil
+        @left = Node.new value, up: self
+        return self
+      else
+        @left.fork value
+      end
+    else
+      if @right === nil
+        @right = Node.new value, up: self
+        return self
+      else
+        @right.fork value
+      end
+    end
+  end
+
+  def is_leaf?
+    @left === nil && @right === nil
+  end
+
+  def inspect
+    value = @value ? "@value=#{@value}" : nil
+    up = @up ? "@up=<Node:#{@up.value}>" : nil
+    left = @left ? "@left=<Node:#{@left.value}>" : nil
+    right = @right ? "@right=<Node:#{@right.value}>" : nil
+    "<Node:#{[value, up, left, right].compact.join(',')}>"
+  end
+
+  def to_s
+    self.inspect
+  end
+end
+
+def build_tree(data, shuffle: true)
+  data = data.to_a.dup
+  data.shuffle! if shuffle
+
+  root = Node.new data.shift
+  data.each { |value| root.fork value }
+  root
+end
+
+def breadth_first_search(target, tree)
+  q = Queue.new
+  q << tree
+  visited = []
+
+  until q.empty?
+    node = q.shift
+    visited << node
+    return node if node.value == target
+
+    [node.up, node.left, node.right]
+        .compact
+        .reject { |n| visited.include? n }
+        .each { |n| q << n }
+  end
+end
+
+def depth_first_search(target, tree)
+  stack = [tree]
+  visited = []
+
+  until stack.empty?
+    node = stack.pop
+    next if visited.include? node
+    return node if node.value == target
+    visited << node
+
+    stack.concat [node.up, node.left, node.right]
+                     .compact
+                     .reject { |n| visited.include? n }
+  end
+end
+
+def dfs_rec target, node, visited: []
+  return node if node.value == target
+  visited << node
+  [node.up, node.left, node.right]
+      .compact
+      .reject { |n| visited.include? n }
+      .collect { |n| dfs_rec(target, n, visited: visited) }
+      .reject { |n| n === nil }
+      .first
+end
--- spec/lib/binary_tree_spec.rb
+++ spec/lib/binary_tree_spec.rb
@@ -2,8 +2,111 @@ require 'spec_helper'
 require 'binary_tree'
 
 describe 'Binary Tree' do
+  describe Node do
+    before(:each) do
+      @empty_node = Node.new
+    end
 
-  it 'should do something' do
-    expect(true).to be false
+    describe "#value" do
+      it "should exist" do
+        expect(@empty_node).to respond_to :value
+      end
+    end
+
+    describe "#parent" do
+      it "should exist" do
+        expect(@empty_node).to respond_to :up
+      end
+    end
+
+    describe "#left" do
+      it "should exist" do
+        expect(@empty_node).to respond_to :left
+      end
+    end
+
+    describe "#right" do
+      it "should exist" do
+        expect(@empty_node).to respond_to :right
+      end
+    end
+
+    describe "#is_leaf?" do
+      it "should exist" do
+        expect(@empty_node).to respond_to :is_leaf?
+      end
+
+      it "should be true if left and right are nil" do
+        expect(@empty_node.is_leaf?).to be true
+
+      end
+
+      it "should be false if left or right aren't nil" do
+        @empty_node.left, @empty_node.right = 1, 2
+        expect(@empty_node.is_leaf?).to be false
+      end
+    end
+  end
+  describe "#build_tree" do
+    before(:each) do
+      @tree = build_tree [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324], shuffle: false
+    end
+
+    it("should build a binary tree") { expect(@tree.value).to eq 1 }
+    it("should build a binary tree") { expect(@tree.right.left.value).to eq 4 }
+    it("should build a binary tree") { expect(@tree.right.right.right.right.value).to eq 6345 }
+    it("should build a binary tree") { expect(@tree.right.right.right.right.up.up.up.left.left.value).to eq 4 }
+  end
+
+  describe "#breadth_first_search" do
+    before(:each) do
+      @tree = build_tree (1..100)
+    end
+
+    it "should return target node" do
+      node = breadth_first_search 50, @tree
+
+      expect(node).to be_truthy
+      expect(node.value).to eq 50
+    end
+
+    it "should return nil if target not found" do
+      expect(breadth_first_search 500, @tree).to be_nil
+    end
+  end
+
+  describe "#depth_first_search" do
+    before(:each) do
+      @tree = build_tree (1..100)
+    end
+
+    it "should return target node" do
+      node = depth_first_search 50, @tree
+
+      expect(node).to be_truthy
+      expect(node.value).to eq 50
+    end
+
+    it "should return nil if target not found" do
+      expect(depth_first_search 500, @tree).to be_nil
+    end
+  end
+
+  describe "#dfs_rec" do
+    before(:each) do
+      @tree = build_tree (1..100)
+    end
+
+    it "should return target node" do
+      node = dfs_rec 50, @tree
+
+      expect(node).to be_truthy
+      expect(node.value).to eq 50
+    end
+
+    it "should return nil if target not found" do
+      expect(dfs_rec 500, @tree).to be_nil
+    end
   end
 end
+
