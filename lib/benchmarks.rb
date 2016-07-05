293d73606f2f5c51ee0e76205bcc51109ffaa90b
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-05 01:32:37 -0500

    include hash benchmark

--- lib/benchmarks.rb
+++ lib/benchmarks.rb
@@ -5,7 +5,7 @@ require_relative 'knight_moves'
 
 test_data = []
 
-50000.times { test_data << (rand 50000) }
+500000.times { test_data << (rand 50000) }
 
 def array_factory(items)
   list = []
@@ -13,11 +13,22 @@ def array_factory(items)
   list
 end
 
+def hash_factory(items)
+  hash = Hash.new
+  items.each { |item| hash[item] = true }
+  hash
+end
+
+test_array, test_tree, test_hash = nil, nil, nil
+
 Benchmark.bm do |benchmark|
-  benchmark.report("test_array push") { $test_array = array_factory(test_data) }
-  benchmark.report("bin tree insert") { $test_tree = BinaryTree.factory(test_data, shuffle: false) }
+  benchmark.report("test_array create") { test_array = array_factory(test_data) }
+  benchmark.report("test_tree  create") { test_tree = BinaryTree.factory(test_data, shuffle: false) }
+  benchmark.report("test_hash  create") { test_hash = hash_factory(test_data) }
 end
 Benchmark.bm do |benchmark|
-  benchmark.report("test_array include") { (1..50000).each { |n| $test_array.include?(n) } }
-  benchmark.report("binary tree search") { (1..50000).each { |n| $test_tree.include?(n) } }
+  benchmark.report("test_array include?     ") { (1..50000).each { |n| test_array.include?(n) } }
+  benchmark.report("test_tree  include? x100") { (1..500000).each { |n| test_tree.include?(n) } }
+  benchmark.report("test_hash  has_key? x100") { (1..500000).each { |n| test_hash.has_key?(n) } }
+
 end
