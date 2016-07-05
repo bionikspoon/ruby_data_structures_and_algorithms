fe902661d779433461cc727a7758f028db29e04f
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-04 19:59:37 -0500

    Implement Binary Tree

--- lib/knight_moves.rb
+++ lib/knight_moves.rb
@@ -1,57 +1,196 @@
-DIM_X, DIM_Y = [(0..7).to_a] * 2
-NAME = Hash[DIM_Y.product(DIM_X).collect { |y, x| [[y, x], ('A'.upto('H').to_a[x] + 8.downto(1).to_a[y].to_s).to_sym] }]
+module Chess
+  DIM_X, DIM_Y = [(0..7).to_a] * 2
+  NAME = Hash[DIM_Y.product(DIM_X).collect { |y, x| [[y, x], ('A'.upto('H').to_a[x] + 8.downto(1).to_a[y].to_s).to_sym] }]
 
-def TO_NAME(y, x)
-  NAME[[y, x]]
-end
+  def to_name(y, x)
+    NAME[[y, x]]
+  end
 
-class Board
-  attr_reader :cells
+  class Board
+    attr_reader :cells
 
-  def initialize
-    @cells = NAME.keys.collect do |y, x|
-      Cell.new y: y, x: x
+    def initialize
+      @cells = NAME.keys.collect do |y, x|
+        Cell.new y: y, x: x
+      end
+
+      @cells.each do |cell|
+        adjacent_cells = { 'up=': [cell.y - 1, cell.x],
+                           'right=': [cell.y, cell.x + 1],
+                           'down=': [cell.y + 1, cell.x],
+                           'left=': [cell.y, cell.x - 1], }
+        adjacent_cells
+            .collect { |k, v| [k, to_name(*v)] }
+            .reject { |_, v| v.nil? }
+            .collect { |k, v| [k, @cells.find { |c| c.name == v }] }
+            .each { |k, v| cell.send(k, v) }
+
+      end
+    end
+  end
+
+  class Cell
+    attr_reader :name, :x, :y
+    attr_accessor :value, :up, :down, :left, :right
+
+    def initialize(y: nil, x: nil, name: nil, value: nil)
+      @y = y
+      @x = x
+      @value = value
+      @name = name || to_name(@y, @x)
+      @up, @right, @down, @left = [nil]*4
     end
 
-    @cells.each do |cell|
-      adjacent_cells = { 'up=': [cell.y - 1, cell.x],
-                         'right=': [cell.y, cell.x + 1],
-                         'down=': [cell.y + 1, cell.x],
-                         'left=': [cell.y, cell.x - 1], }
-      adjacent_cells
-          .collect { |k, v| [k, TO_NAME(*v)] }
-          .reject { |_, v| v.nil? }
-          .collect { |k, v| [k, @cells.find { |c| c.name == v }] }
-          .each { |k, v| cell.send(k, v) }
+    def inspect
+      s = []
+      s << "*: #{to_s}"
+      s << "u: #{@up.to_s}" if @up
+      s << "r: #{@right.to_s}" if @right
+      s << "d: #{@down.to_s}" if @down
+      s << "l: #{@left.to_s}" if @left
+      "{#{s.join(', ')}}"
+    end
 
+    def to_s
+      value = @value ? ":{#{@value}" : nil
+      "<#{@name}#{value}>"
     end
   end
 end
 
-class Cell
-  attr_reader :name, :x, :y
-  attr_accessor :value, :up, :down, :left, :right
 
-  def initialize(y: nil, x: nil, name: nil, value: nil)
-    @y = y
-    @x = x
-    @value = value
-    @name = name || TO_NAME(@y, @x)
-    @up, @right, @down, @left = [nil]*4
+module BinaryTree
+  class Node
+    include Enumerable
+
+    attr_accessor :right, :value, :up, :left
+
+    def initialize(value=nil, up: nil, left: nil, right: nil)
+      @value = value
+      @up = up || EmptyNode.new
+      @left = left || EmptyNode.new
+      @right = right || EmptyNode.new
+    end
+
+    def each
+      return enum_for :each unless block_given?
+
+      left.each { |node| yield node }
+      yield value
+      right.each { |node| yield node }
+    end
+
+    def insert value, up: nil
+      up ||= self.class.new up
+
+      unless up?
+        @value = value
+        @up = up
+        return self
+      end
+
+      case self.value <=> value
+        when -1 then
+          insert_right(value)
+        else
+          insert_left(value)
+      end
+
+      self.up || self
+    end
+
+    def each_node
+      return enum_for :each_node unless block_given?
+
+      left.each_node { |node| yield node }
+      yield self
+      right.each_node { |node| yield node }
+    end
+
+    def nodes
+      each_node.to_a
+    end
+
+    def <=> other
+      [value, up] <=> [other.value, other.up]
+    end
+
+    def inspect
+      "{Node:#{@value}:#{@left.inspect}:#{@right.inspect}:#{@up.inspect}}"
+    end
+
+    def to_s
+      "{Node:#{value}:#{@left.value}:#{@right.value}:#{@up.value}}"
+    end
+
+    protected
+    def insert_left(value)
+      left.insert(value) or self.left = Node.new(value, up: self)
+    end
+
+    def insert_right(value)
+      right.insert(value) or self.right = Node.new(value, up: self)
+    end
+
+    def is_leaf?
+      !(left? or right?)
+    end
+
+    def up?
+      up.is_a? self.class
+    end
+
+    def left?
+      left.is_a? self.class
+    end
+
+    def right?
+      right.is_a? self.class
+    end
+
   end
+  class EmptyNode
+    include Enumerable
 
-  def inspect
-    s = []
-    s << "*: #{to_s}"
-    s << "u: #{@up.to_s}" if @up
-    s << "r: #{@right.to_s}" if @right
-    s << "d: #{@down.to_s}" if @down
-    s << "l: #{@left.to_s}" if @left
-    "{#{s.join(', ')}}"
-  end
+    attr_reader :right, :value, :left, :up
 
-  def to_s
-    value = @value ? ":{#{@value}" : nil
-    "<#{@name}#{value}>"
+    def initialize(*)
+      @value = nil
+      @left = self
+      @right = self
+      @up = self
+    end
+
+    def each
+      enum_for :each unless block_given?
+    end
+
+    def each_node
+      enum_for :each_node unless block_given?
+    end
+
+    def insert(*)
+      false
+    end
+
+    def up?
+      false
+    end
+
+    def left?
+      false
+    end
+
+    def right?
+      false
+    end
+
+    def inspect
+      "{#{self.class}}"
+    end
+
+    def to_s
+      inspect
+    end
   end
 end
--- spec/lib/knight_moves_spec.rb
+++ spec/lib/knight_moves_spec.rb
@@ -1,6 +1,7 @@
 require 'knight_moves'
+include Chess, BinaryTree
 
-describe 'Knight Moves' do
+describe Chess do
   describe Board do
     context Board.new do
       its('cells.length') { should eq 64 }
@@ -23,5 +24,124 @@ describe 'Knight Moves' do
     its([[3, 4]]) { should be :E5 }
     its([[9, 9]]) { should be_nil }
   end
+
+  describe :TO_NAME do
+    it("should convert (y,x) to cell name") { expect(to_name(4, 3)).to be :D4 }
+  end
 end
 
+describe BinaryTree do
+  describe Node do
+    shared_context(:empty) { subject { Node.new } }
+    shared_context(:words) { subject { Node.new 'value', up: 'up', left: 'left', right: 'right' } }
+    shared_context(:numbers) do
+      subject do
+        node = Node.new(4)
+        left = Node.new(3, up: node)
+        right = Node.new(5, up: node)
+        node.left, node.right = left, right
+        node
+      end
+    end
+
+    describe '#initialize' do
+      include_context :empty do
+        it { should be_truthy }
+        it { should respond_to :value }
+        it { should respond_to :up }
+        it { should respond_to :left }
+        it { should respond_to :right }
+      end
+
+      context 'with values' do
+        include_context :words
+        its(:value) { should eql 'value' }
+        its(:up) { should eql 'up' }
+        its(:left) { should eql 'left' }
+        its(:right) { should eql 'right' }
+      end
+    end
+
+    describe '#each' do
+      include_context :numbers
+      its('each.to_a') { should eq [3, 4, 5] }
+    end
+
+    describe '#each_node' do
+      include_context :numbers
+      its('each_node.to_a') { should eq [subject.left, subject, subject.right] }
+    end
+
+    describe '#nodes' do
+      include_context :numbers
+      its(:nodes) { should eq [subject.left, subject, subject.right] }
+    end
+
+    describe '#is_leaf?' do
+      include_context :numbers
+      its(:is_leaf?) { should be false }
+      its('left.is_leaf?') { should be true }
+      its('right.is_leaf?') { should be true }
+      its('left.up.is_leaf?') { should be false }
+
+    end
+
+    describe "#<=>" do
+      include_context :numbers
+      it { expect(subject <=> Node.new(3)).to be 1 }
+    end
+
+    describe '#insert' do
+      include_context :empty
+      context '1 item' do
+        before { subject.insert (5) }
+        its(:value) { should be 5 }
+      end
+      context '2 items' do
+        before do
+          subject.insert(5)
+          subject.insert(4)
+        end
+        its('left.value') { should be 4 }
+
+      end
+
+
+      it "should keep items sorted" do
+        subject.insert(5).insert(4).insert(7).insert(6)
+
+        expect(subject)
+
+      end
+    end
+
+
+    describe 'enum methods' do
+      include_context :numbers
+
+      describe '#inject' do
+        it { expect(subject.inject(0) { |memo, value| memo + value }).to be 12 }
+      end
+
+      describe '#max' do
+        its(:max) { should be 5 }
+      end
+    end
+  end
+  describe EmptyNode do
+    shared_context(:empty) { subject { EmptyNode.new } }
+    describe '#initialize' do
+      include_context :empty
+      its(:value) { should be_nil }
+      its(:up) { should be subject }
+      its(:left) { should be subject }
+      its(:right) { should be subject }
+      its(:up?) { should be false }
+      its(:left?) { should be false }
+      its(:right?) { should be false }
+      its(:insert) { should be false }
+      its(:inspect) { should eql '{BinaryTree::EmptyNode}' }
+      its(:to_s) { should eql '{BinaryTree::EmptyNode}' }
+    end
+  end
+end
